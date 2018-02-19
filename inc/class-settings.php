cea82b8b754c000be1c0f1e402d381090ea690cc
bdolor <bpayne@bccampus.ca>
2018-02-19 15:13:24 -0800

    adding autoloader and settings class

---
+++ inc/class-settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Project: custom-wp-notify
+ * Project Sponsor: BCcampus <https://bccampus.ca>
+ * Date: 2018-02-19
+ * Licensed under GPLv3, or any later version
+ *
+ * @license https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+namespace BCcampus;
+
+
+class Settings {
+
+}
