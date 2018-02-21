d3e58a8fc8442752d82c85362089a2888689698a
Alex Paredes <alexparedes@Alexs-MacBook-Pro.local>
2018-02-21 14:29:56 -0800

    Code comments with DocBlocks

--- inc/class-settings.php
+++ inc/class-settings.php
@@ -22,15 +22,21 @@ class Settings {
 		add_shortcode( 'cwp_notify', array( $this, 'cwp_shortcode' ) );
 	}
 
+	/**
+	 * Add admin menu to dashboard
+	 */
 	function cwp_add_admin_menu() {
 
-		add_options_page( 'custom-wp-notify', 'custom-wp-notify', 'manage_options', 'custom-wp-notify', array(
+		add_options_page( 'Custom WP Notify', 'Custom WP Notify', 'manage_options', 'custom-wp-notify', array(
 			$this,
 			'cwp_options_page'
 		) );
 
 	}
 
+	/**
+	 * Register the plugin settings, create fields
+	 */
 	function cwp_settings_init() {
 
 		register_setting( 'cwpOptions', 'cwp_settings' );
@@ -77,6 +83,9 @@ class Settings {
 
 	}
 
+	/**
+	 * Render the options page enable field
+	 */
 	function cwp_enable_render() {
 
 		$options = get_option( 'cwp_settings' );
@@ -86,6 +95,9 @@ class Settings {
 
 	}
 
+	/**
+	 * Render the options page frequency field
+	 */
 	function cwp_frequency_render() {
 
 		$options = get_option( 'cwp_settings' );
@@ -100,6 +112,9 @@ class Settings {
 
 	}
 
+	/**
+	 * Render the options page opt-in field
+	 */
 	function cwp_optin_render() {
 
 		$options = get_option( 'cwp_settings' );
@@ -109,6 +124,9 @@ class Settings {
 
 	}
 
+	/**
+	 * Render the options page template field
+	 */
 	function cwp_template_render() {
 
 		$options = get_option( 'cwp_settings' );
@@ -119,6 +137,9 @@ class Settings {
 
 	}
 
+	/**
+	 * The function to be called to output the content for this page
+	 */
 	function cwp_options_page() {
 
 		?>
@@ -137,19 +158,26 @@ class Settings {
 
 	}
 
-	// Create the [cpw_notify] shortcode
+	/**
+	 * @param $atts
+	 * Contents of the shortcode
+	 *
+	 * @return string
+	 */
 	function cwp_shortcode( $atts ) {
 
 		// Get the users stored preference
 		$user_id = get_current_user_id();
 		$pref    = get_user_meta( $user_id, 'cwp_notify', true );
+		// Get the value, set to 0 as default
 		( $pref ) ? $prefvalue = $pref : $prefvalue = 0;
 
-		// Get the prefix text from plugin options
+		// Get prefix text for checkbox from the plugin options
 		$getoptions = get_option( 'cwp_settings' );
-		// Set default prefix text if none exists
+		// Set default prefix text for the checkbox if none exists
 		( $getoptions['cwp_notify'] ) ? $usertext = $getoptions['cwp_notify'] : $usertext = "Subscribe to Notifications";
 
+		// The checkbox with prefix text from options page, and the user value of cwp_notify
 		$html = $usertext . '<input type="checkbox" name="cwp-opt-in" value="' . $prefvalue . '">';
 
 		return $html;
