102341f3bf5890ed7cd52dfb03ded27fa2689317
Alex Paredes <alexparedes@Alexs-MacBook-Pro.local>
2018-02-21 15:28:28 -0800

    rm shortcode from settings class

--- inc/class-settings.php
+++ inc/class-settings.php
@@ -157,43 +157,5 @@ class Settings {
 		<?php
 
 	}
-
-	/**
-	 * @param $atts
-	 * Contents of the shortcode
-	 *
-	 * @return string
-	 */
-	function cwpShortCode( $atts ) {
-
-		// Get the users stored preference
-		$user_id = get_current_user_id();
-		$pref    = get_user_meta( $user_id, 'cwp_notify', true );
-		// Get the value, set to 0 as default
-		( $pref ) ? $prefvalue = $pref : $prefvalue = 0;
-
-		// Get prefix text for checkbox from the plugin options
-		$getoptions = get_option( 'cwp_settings' );
-		// Set default prefix text for the checkbox if none exists
-		( $getoptions['cwp_notify'] ) ? $usertext = $getoptions['cwp_notify'] : $usertext = "Subscribe to Notifications";
-
-		// The checkbox with prefix text from options page, and the user value of cwp_notify
-		$html = $usertext . '<input type="checkbox" name="cwp-opt-in" value="' . $prefvalue . '">';
-
-		return $html;
-	}
 }
 
-/**
- *  Update/Create user meta cwp_notify
- */
-function cwpOptIn() {
-
-	// Save save opt-in preference on form submission
-	if ( isset( $_POST['cwp-opt-in'] ) ) {
-		$user_id = get_current_user_id();
-		$opt     = $_POST['cwp-opt-in'];
-
-		update_user_meta( $user_id, 'cwp_notify', $opt );
-	}
-}
