4ad341a06660bb9f56eda5b65f836151321e8bc4
Alex Paredes <alexparedes@Alexs-MacBook-Pro.local>
2018-02-22 14:49:55 -0800

    almost ajax

--- inc/class-shortcode.php
+++ inc/class-shortcode.php
@@ -17,6 +17,9 @@ class Shortcode {
 	 */
 	function __construct() {
 		add_shortcode( 'cwp_notify', [ $this, 'cwpShortCode' ] );
+		add_action( 'wp_enqueue_scripts', [ $this, 'cwpScripts' ] );
+		add_action( 'wp_ajax_nopriv_save_notification', [ $this, 'cwpOptIn' ] );
+		add_action( 'wp_ajax_save_notification', [ $this, 'cwpOptIn' ] );
 	}
 
 	/**
@@ -38,26 +41,50 @@ class Shortcode {
 		// Set default prefix text for the checkbox if none exists
 		( $getoptions['cwp_notify'] ) ? $usertext = $getoptions['cwp_notify'] : $usertext = 'Subscribe to Notifications';
 
+		// Compatible AJAX request for WordPress
+		$url = admin_url( 'admin-ajax.php' );
+
 		// The checkbox with prefix text from options page, and the user value of cwp_notify
-		$html = $usertext . '<input type="checkbox" name="cwp-opt-in" value="' . $prefvalue . '">';
+		$html = '<form class="cwp-notify" method="post" action="' . $url . '">';
+		$html .= $usertext . '<input class="notify" type="checkbox" name="cwp-opt-in" value="' . $prefvalue . '">';
+		$html .= '<p class="cwp-notify-response"></p>';
+		$html .= '<input type="hidden" name="action" value="custom_action">';
 		$html .= wp_nonce_field( 'notify_preference', 'submit_notify_preference' );
+		$html .= '<button>Send</button>';
 
 		return $html;
 	}
 
-
 	/**
-	 *  Update/Create user meta cwp_notify
+	 *  AJAX callback to update/create user meta
 	 */
 	function cwpOptIn() {
 
-		// Save save opt-in preference on form submission
-		if ( isset( $_POST['cwp-opt-in'] ) && check_admin_referer( 'notify_preference', 'submit_notify_preference' ) ) {
+		// Check nonce is valid and save opt-in preference if user is logged in
+		if ( is_user_logged_in() && isset( $_POST['cwp-opt-in'] ) && check_admin_referer( 'notify_preference', 'submit_notify_preference' ) ) {
 			$user_id = get_current_user_id();
 			$opt     = $_POST['cwp-opt-in'];
-
+			// create or update meta
 			update_user_meta( $user_id, 'cwp_notify', $opt );
+
+			wp_send_json_success( __( 'Thanks for subscribing!', 'reportabug' ) );
+
+		} else {
+
+			wp_send_json_success( __( 'Please Log in or Register to use this feature.', 'reportabug' ) );
 		}
+
+		return;
 	}
 
+	/**
+	 * Enqueue scripts and styles
+	 */
+	function cwpScripts() {
+		wp_enqueue_script( 'cwp-notify', plugin_dir_url( __FILE__ ) . 'assets/scripts/cwp-notify.js', [ 'jquery' ], null, true );
+		wp_enqueue_style( 'cwp-notify', plugin_dir_url( __FILE__ ) . 'assets/css/style.css' );
+		wp_localize_script( 'cwp-notify', 'settings', [ 'ajaxurl' => admin_url( 'admin-ajax.php' ) ] );
+	}
 }
+
+
